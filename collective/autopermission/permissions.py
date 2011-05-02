a2111777e165917ea822b0ed510f1856ca849908
jfroche <jfroche@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-02 13:01:04 +0000

    Preparing release 1.0b2

--- collective/autopermission/permissions.py
+++ collective/autopermission/permissions.py
@@ -1,18 +1,20 @@
 import Products
 from AccessControl.Permission import _registeredPermissions
 from AccessControl.Permission import pname
-from AccessControl.Permission import ApplicationDefaultPermissions 
+from AccessControl.Permission import ApplicationDefaultPermissions
 
 # This is borrowed from Products.CMFCore to avoid a dependency.
 
+
 def setDefaultRoles(permission, roles):
     registered = _registeredPermissions
-    if not registered.has_key(permission):
+    if permission not in registered:
         registered[permission] = 1
-        Products.__ac_permissions__=(
-            Products.__ac_permissions__+((permission,(),roles),))
+        Products.__ac_permissions__ = (
+            Products.__ac_permissions__ + ((permission, (), roles),))
         mangled = pname(permission)
         setattr(ApplicationDefaultPermissions, mangled, roles)
+
 
 def create_permission(permission, event):
     """When a new IPermission utility is registered (via the <permission />
